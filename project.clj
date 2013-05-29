736d93be4e4f65803cf18a1b4debea8aba88b244
Benedek Fazekas <benedek.fazekas@mailonline.co.uk>
2013-05-29 17:52:53 +0100

    nexus config

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "https://github.com/benedekfazekas/clj_manifest.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :repositories {"snapshots" {:url "http://10.251.76.32:8081/nexus/content/repositories/snapshots"
+                              :username "admin" :password "admin123"}
+                 "releases" {:url "http://10.251.76.32:8081/nexus/content/repositories/releases"
+                             :username "admin" :password "admin123" }
+                 "thirdparty" {:url "http://10.251.76.32:8081/nexus/content/repositories/thirdparty"}})
